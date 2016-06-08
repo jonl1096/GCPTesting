a2fdc11a296fcc850b1542a7a44813e7ced02199
jonl1096 <jonathan.jy.liu@gmail.com>
2016-06-08 14:23:34 -0400

    trying to implement google sign in stuff

--- app/src/main/java/com/example/jliu/gcp/GcpManager.java
+++ app/src/main/java/com/example/jliu/gcp/GcpManager.java
@@ -1,6 +1,18 @@
 package com.example.jliu.gcp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.AccountPicker;
+import com.google.android.gms.plus.Plus;
+
+import java.io.IOException;
 
 /**
  * Created by jliu on 6/7/2016.
@@ -9,8 +21,86 @@ public class GcpManager {
 
     private Context context;
 
+    private static final String TAG = "RetrieveAccessToken";
+
+    private static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
+    private static final int REQ_SIGN_IN_REQUIRED = 55664;
+    private String mAccountName;
+
     public GcpManager(Context pContext, String pEmail, String pPassword) {
         this.context = pContext;
+
+//         Manual integration? Pop an account chooser to get this:
+//        mAccountName = "jliu@newsamerica.com";
+//         Or if you have a GoogleApiClient connected:
+//        mAccountName = Plus.AccountApi.getAccountName(mGoogleApiClient);
+//        mAccountName = GoogleSignInOptions.Builder.requestServerAuthCode
+//
+//        RetrieveTokenTask retriever = new RetrieveTokenTask();
+//
+//        retriever.execute(mAccountName);
+
     }
+
+
+//    private void pickUserAccount() {
+//        String[] accountTypes = new String[]{"com.google"};
+//        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+//                accountTypes, false, null, null, null, null);
+//        this.context.startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
+//    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_retrieve_access_token);
+//        findViewById(R.id.button_token).setOnClickListener(this);
+//
+//        // Manual integration? Pop an account chooser to get this:
+//        mAccountName = "jliu@newsamerica.com";
+//        // Or if you have a GoogleApiClient connected:
+//        mAccountName = Plus.AccountApi.getAccountName(mGoogleApiClient);
+//    }
+
+//    @Override
+//    public void onClick(View view) {
+//        if (view.getId() == R.id.button_token) {
+//            new RetrieveTokenTask().execute(mAccountName);
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQ_SIGN_IN_REQUIRED && resultCode == RESULT_OK) {
+//            // We had to sign in - now we can finish off the token request.
+//            new RetrieveTokenTask().execute(mAccountName);
+//        }
+//    }
+
+//    private class RetrieveTokenTask extends AsyncTask<String, Void, String> {
+//
+//        @Override
+//        protected String doInBackground(String... params) {
+//            String accountName = params[0];
+//            String scopes = "oauth2:profile email";
+//            String token = null;
+//            try {
+//                token = GoogleAuthUtil.getToken(getApplicationContext(), accountName, scopes);
+//            } catch (IOException e) {
+//                Log.e(TAG, e.getMessage());
+//            } catch (UserRecoverableAuthException e) {
+//                this.context.startActivityForResult(e.getIntent(), REQ_SIGN_IN_REQUIRED);
+//            } catch (GoogleAuthException e) {
+//                Log.e(TAG, e.getMessage());
+//            }
+//            return token;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String s) {
+//            super.onPostExecute(s);
+////            ((TextView) findViewById(R.id.token_value)).setText("Token Value: " + s);
+//        }
+//    }
 
 }
--- app/src/main/java/com/example/jliu/gcp/MainActivity.java
+++ app/src/main/java/com/example/jliu/gcp/MainActivity.java
@@ -1,31 +1,59 @@
 package com.example.jliu.gcp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
 
     private static final String IMAGE_URL =
             "http://www.wordmstemplates.com/wp-content/uploads/2015/08/Coupon-Template-2154.jpg";
-
     private static final String PDF_URL =
             "http://www.cs.jhu.edu/~lixints/class/auto/HW1.pdf";
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private static final int REQ_SIGN_IN_REQUIRED = 55664;
+    private String mAccountName;
+
+    private GoogleApiClient mGoogleApiClient;
+    private TextView mStatusTextView;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,10 +69,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 // Get the uri from url string
                 Uri docUri = Uri.parse(PDF_URL);
-
                 //Build an intent to launch to print
                 Intent printIntent = new Intent(MainActivity.this, PrintDialogActivity.class);
                 printIntent.setDataAndType(docUri, "application/pdf");
@@ -52,6 +78,219 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(printIntent);
             }
         });
+
+        //=====================================================================================
+
+        // Views
+        mStatusTextView = (TextView) findViewById(R.id.textView);
+
+        // Button listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.disconnect_button).setOnClickListener(this);
+
+        // [START configure_signin]
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(MainActivity.this.getResources().getString(R.string.server_client_id))
+                .requestEmail()
+                .build();
+        // [END configure_signin]
+
+        // [START build_client]
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+        // [END build_client]
+
+        // [START customize_button]
+        // Customize sign-in button. The sign-in button can be displayed in
+        // multiple sizes and color schemes. It can also be contextually
+        // rendered based on the requested scopes. For example. a red button may
+        // be displayed when Google+ scopes are requested, but a white button
+        // may be displayed when only basic profile is requested. Try adding the
+        // Scopes.PLUS_LOGIN scope to the GoogleSignInOptions to see the
+        // difference.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
+        // [END customize_button]
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    hideProgressDialog();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
+    // [START onActivityResult]
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+    // [END onActivityResult]
+
+    // [START handleSignInResult]
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            mStatusTextView.setText(getString(R.string.signed_in_fmt, acct.getDisplayName()));
+            updateUI(true);
+        } else {
+            // Signed out, show unauthenticated UI.
+            updateUI(false);
+        }
+    }
+    // [END handleSignInResult]
+
+    // [START signIn]
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    // [END signIn]
+
+    // [START signOut]
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        // [START_EXCLUDE]
+                        updateUI(false);
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END signOut]
+
+    // [START revokeAccess]
+    private void revokeAccess() {
+        Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        // [START_EXCLUDE]
+                        updateUI(false);
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END revokeAccess]
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+//            findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+//            findViewById(R.id.sign_out_button).setVisibility(View.GONE);
+//            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.sign_out_button:
+                signOut();
+                break;
+            case R.id.disconnect_button:
+                revokeAccess();
+                break;
+            case R.id.tokenButton:
+                RetrieveTokenTask retriever = new RetrieveTokenTask();
+                retriever.execute(mAccountName);
+                break;
+        }
+    }
+    private class RetrieveTokenTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String accountName = params[0];
+            String scopes = "oauth2:profile email";
+            String token = null;
+            try {
+                token = GoogleAuthUtil.getToken(getApplicationContext(), accountName, scopes);
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            } catch (UserRecoverableAuthException e) {
+                startActivityForResult(e.getIntent(), REQ_SIGN_IN_REQUIRED);
+            } catch (GoogleAuthException e) {
+                Log.e(TAG, e.getMessage());
+            }
+            return token;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+//            ((TextView) findViewById(R.id.token_value)).setText("Token Value: " + s);
+        }
     }
 
     private class ImageLoader extends AsyncTask<String, Void, Bitmap> {
@@ -63,8 +302,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Bitmap b) {
-            ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            imageView.setImageBitmap(b);
+//            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//            imageView.setImageBitmap(b);
         }
 
         @Override
